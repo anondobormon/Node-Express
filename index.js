dcd6d02435439eedc33e72878ecc1f778ba6c2b0
anondobormon <anondokpi@gmail.com>
2021-08-08 22:14:04 +0600

    express object

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.raw());
+app.use(express.text());
+app.use(express.urlencoded());
+
+app.use(
+  express.static(`${__dirname}/public/`, {
+    index: "home.html",
+  })
+);
+
+const router = express.Router({
+  caseSensitive: true,
+});
+app.use(router);
+
+router.get("/home", (req, res) => {
+  res.send("This is Router function work");
+});
+
 app.get("/", (req, res) => {
   res.send("This is home page");
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body);
   res.send("This is home with post request");
 });
 
