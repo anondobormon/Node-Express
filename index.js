e69cbfe35168d05f29a6fc952f2bff92751ccb85
anondobormon <anondokpi@gmail.com>
2021-08-09 23:41:20 +0600

    app-mountpath

--- index.js
+++ index.js
@@ -2,34 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.raw());
-app.use(express.text());
-app.use(express.urlencoded());
-
-app.use(
-  express.static(`${__dirname}/public/`, {
-    index: "home.html",
-  })
-);
-
-const router = express.Router({
-  caseSensitive: true,
+// app.mountpath method
+const admin = express();
+admin.get("/dashboard", (req, res) => {
+  console.log(admin.mountpath);
+  res.send("Admin mountpath");
 });
-app.use(router);
-
-router.get("/home", (req, res) => {
-  res.send("This is Router function work");
-});
-
-app.get("/", (req, res) => {
-  res.send("This is home page");
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("This is home with post request");
-});
+app.use("/admin", admin);
 
 app.listen(3000, () => {
   console.log("Listing on port 3000");
